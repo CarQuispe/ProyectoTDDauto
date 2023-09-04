f382ca6f6c5347d05a3084251a06952dd8ece7e5
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-09-04 01:19:10 -0400

    girar Izquierda

--- src/carro.js
+++ src/carro.js
@@ -1,22 +1,46 @@
-// Definición de la clase Carro
-class Carro {
+import createGrid from "./autoControlador.js";
+
+export class Carro { // Cambio en la exportación
   constructor(x, y, direccion) {
     this.x = x;
     this.y = y;
-    
-    if (direccion === 'E' || direccion === 'O' || direccion === 'N' || direccion === 'S') {
+
+    if (direccion == 'E' || direccion == 'O' || direccion == 'N' || direccion == 'S') {
       this.direccion = direccion;
     } else {
       throw new Error('Dirección no válida. Debe ser E, O, N o S.');
     }
   }
+
+  girarIzquierda() {
+    if (this.direccion === "N") {
+      this.direccion = "O";
+    } else if (this.direccion === "O") {
+      this.direccion = "S";
+    } else if (this.direccion === "S") {
+      this.direccion = "E";
+    } else if (this.direccion === "E") {
+      this.direccion = "N";
+    }
+  }
+ 
+  getPosicion() {
+    return `${this.x},${this.y} ${this.direccion}`;
+  }
+
+  moveForward() {
+    if (this.direccion === "N") {
+      this.y++;
+    } else if (this.direccion === "S") {
+      this.y--;
+    } else if (this.direccion === "E") {
+      this.x++;
+    } else if (this.direccion === "O") {
+      this.x--;
+    }
+  }
 }
 
-// Función para crear un carro
-function crearCarro(x, y, direccion) {
-  return new Carro(x, y, direccion);
-}
 
-// Exportar la función y la clase
-module.exports = { crearCarro, Carro };
+
 
